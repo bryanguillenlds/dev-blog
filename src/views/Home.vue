cc374c4197f586c734c09196d94edc3eba7d257d
Bryan Guillen <bryan.guillen.lds@gmail.com>
2022-06-05 20:37:26 -0600

    Added a CTA for Sign Up in the Home View

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,16 +10,25 @@
         </div>
       </div>
     </div>
+    <div class="updates">
+      <div class="container">
+        <h2>Never Miss a Post. Sign Up for a <span>Free Account</span> today!</h2>
+        <router-link class="router-button" to="#">
+          Sign Up to my BlogFolio <Arrow class="arrow arrow-light"/>
+        </router-link>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import BlogPost from '../components/BlogPost.vue';
 import BlogCard from '../components/BlogCard.vue';
+import Arrow from '../assets/Icons/arrow-right-light.svg';
 
 export default {
   name: "Home",
-  components: { BlogPost, BlogCard },
+  components: { BlogPost, BlogCard, Arrow },
   data() {
     return {
       welcomeScreen: {
@@ -58,6 +67,48 @@ export default {
     font-weight: 300;
     font-size: 28px;
     margin-bottom: 32px;
+  }
+}
+
+.updates {
+  .container {
+    padding: 100px 25px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    @media (min-width: 850px) {
+      padding: 125px 25px;
+      flex-direction: row;
+    }
+
+    .router-button {
+      display: flex;
+      font-size: 14px;
+      text-decoration: none;
+
+      @media (min-width: 850px) {
+        margin-left: auto;
+      }
+    }
+
+    h2 {
+      font-weight: 300;
+      font-size: 32px;
+      max-width: 425px;
+      width: 100%;
+      text-align: center;
+      text-transform: uppercase;
+
+      span {
+        color: #1eb863;
+      }
+
+      @media (min-width: 850px) {
+        text-align: initial;
+        font-size: 40px;
+      }
+    }
   }
 }
 </style>
