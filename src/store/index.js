fa6ac08eed23d1c924d2df462cedd21f9d311b39
bryan <bryanguillenlds@gmail.com>
2023-03-27 16:31:53 -0600

    Finished action for getting current user information for their profile

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import firebase from 'firebase/app'
+import 'firebase/auth';
+import db from '../firebase/firebaseInit.js';
 
 Vue.use(Vuex)
 
@@ -11,14 +14,44 @@ export default new Vuex.Store({
       { blogTitle: "Sample Blog #3", blogCoverPhoto: "stock-3", blogDate: "May 30, 2022" },
       { blogTitle: "Sample Blog #4", blogCoverPhoto: "stock-4", blogDate: "May 30, 2022" }
     ],
-    editPost: null
+    editPost: null,
+    user: null,
+    profileEmail: null,
+    profileFirstName: null,
+    profileLastName: null,
+    profileUsername: null,
+    profileId: null,
+    profileInitials: null
   },
   mutations: {
     toggleEditPost(state, payload) {
       state.editPost = payload;
+    },
+    updateUser(state, payload) {
+      state.user = payload;
+    },
+    setProfileInfo(state, payload) {
+      state.profileId = payload.id;
+      state.profileEmail = payload.data().email; //have to use data() from firebase to get this info
+      state.profileFirstName = payload.data().firstName;
+      state.profileLastName = payload.data().lastName;
+      state.profileUsername = payload.data().username;
+    },
+    setProfileInitials(state) {
+      state.profileInitials =
+          state.profileFirstName.match(/(\b\S)?/g).join('') +
+          state.profileLastName.match(/(\b\S)?/g).join('');
     }
   },
   actions: {
+    async getCurrentUser({commit}) {
+      //get the current user doc from firebase, passing the current user id
+      const database = await db.collection('users').doc(firebase.auth().currentUser.uid);
+      const dbResult = await database.get();
+
+      commit('setProfileInfo', dbResult);
+      commit('setProfileInitials');
+    }
   },
   modules: {
   }
